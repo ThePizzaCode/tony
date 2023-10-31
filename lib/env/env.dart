e30039b394d8b9250ad1532d2c33f8a9e616152d
Gabriel Stanciu <57194563+ThePizzaCode@users.noreply.github.com>
2023-10-31 18:47:59 +0200

    deleted files

---
+++ lib/env/env.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const Color tonyOrange = Color(0xFFFD9800);
+const Color lightGrey = Color(0xFFE8E8E8);
+const Color darkGrey = Color(0XFF6E6E6E);
+
+
+//Variables
